e9b069abc257ae402bd583b18345a651126b6e43
Leonardo de Sousa <leoleandrohtc@gmail.com>
2021-12-02 11:48:47 +0100

    Day 01 - Completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,56 @@
 fun main() {
+    fun analyseInput(input: List<String>) {
+        var incrementalCount = 0
+        var sumIncrementalCount = 0
+        val sumOfThree = mutableListOf<Int>()
+
+        input.forEachIndexed { i, measurement ->
+            val intVal = measurement.toInt()
+            var previousSum: Int = Int.MAX_VALUE
+
+            if (sumOfThree.size >= 3) {
+                previousSum = sumOfThree.sum()
+                print("$previousSum $sumOfThree (<- $measurement) | ")
+                sumOfThree.removeFirst()
+            }
+            sumOfThree.add(intVal)
+            if (sumOfThree.sum() > previousSum) {
+                sumIncrementalCount++
+                println("${sumOfThree.sum()} $sumOfThree ($sumIncrementalCount)")
+            } else
+            println("${sumOfThree.sum()} $sumOfThree")
+
+            if (i != 0 && intVal > input[i - 1].toInt()) {
+                incrementalCount++
+            }
+
+            /*if (i == 0) {
+                println("$intVal (N/A - no previous measurement)")
+            } else if (intVal > input[i - 1].toInt()) {
+                incrementalCount++
+                println("$intVal (increased) ($incrementalCount)")
+            } else {
+                println("$intVal (decreased)")
+            }*/
+        }
+
+        println("Incremental count: $incrementalCount | Sum incremental count: $sumIncrementalCount.")
+    }
+
     fun part1(input: List<String>): Int {
+        analyseInput(input)
         return input.size
     }
 
     fun part2(input: List<String>): Int {
+        analyseInput(input)
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 10)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    part2(input)
 }
